483e2075ae9fe3155b95b498f07124c55e936dcf
James <jpnock@gmail.com>
2023-02-24 01:27:06 +0000

    inference: Add width-inference for Verification components

--- src/Renderer/Common/WidthInferer.fs
+++ src/Renderer/Common/WidthInferer.fs
@@ -164,10 +164,34 @@ let private calculateOutputPortsWidth
         InputPortNumber >> (getConnectionIdForPort inputConnectionsWidth)
     match comp.Type with
     | Verification vComp ->
-        // TODO(jpnock): Support width inference.
-        let todo = "TODO(jpnock): Support width inference"
-        printf $"{todo}"
-        Ok <| Map.empty
+        let inputWidths =
+            inputConnectionsWidth
+            |> Map.map ( fun _ v ->
+                Option.map fst v
+                |> Option.defaultValue None)
+        
+        let notAllWidthsValid =
+            inputWidths
+            |> Map.exists (fun _ -> Option.isNone)
+        
+        // TODO(jpnock): Tidy
+        if notAllWidthsValid then
+            Ok <| Map.empty
+        else
+            let emptyInputWidths: Map<int, int> = Map.empty
+      
+            let convertedInputWidths =
+                (emptyInputWidths, inputWidths) ||> Map.fold (
+                    fun state k v -> (
+                        match k with InputPortNumber inputNum -> Map.add inputNum (Option.get v) state
+                    )
+                )
+
+            Verification.Components.outputPortWidths vComp convertedInputWidths
+            |> List.mapi (fun idx outputWidth ->
+                (getOutputPortId comp idx, outputWidth))
+            |> Map.ofList
+            |> Ok
     | ROM _ | RAM _ | AsyncROM _ ->
         failwithf "What? Legacy RAM component types should never occur"
     | Input _ ->

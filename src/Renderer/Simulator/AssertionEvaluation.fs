e55c02f99d72629ff95237d627d6c4e26a47cba7
lupisdupis <61511096+lucianecchi@users.noreply.github.com>
2023-02-26 21:20:33 +0000

    integrated simulator results in evaluation

--- src/Renderer/Simulator/AssertionEvaluation.fs
+++ src/Renderer/Simulator/AssertionEvaluation.fs
@@ -3,6 +3,7 @@ module AssertionEvaluation
 open AssertionTypes
 open AssertionCheck
 open CommonTypes 
+open SimulatorTypes
 
 // these types are here because they concern evaluation, not assertions in general
 type IntToBool = int -> int -> bool
@@ -37,8 +38,21 @@ let boolToUint =
 let intToBool n = if n = 0 then false else true
 let uintToBool n = if n = uint 0 then false else true
 
+//get FComponentId for component that is in the sheet where it's currently being simulated 
+let getFComponentId label components = 
+    let isRightComponent (comp: Component) = 
+        match comp.Label, comp.Type with 
+        | labelComp, Viewer _ when labelComp = label-> Some(comp.Id)
+        | _ -> None 
+    let compId = 
+        List.choose isRightComponent components 
+        |> function 
+            | a::[] -> a
+            | _ -> failwithf "should not happen"
+    FComponentId (ComponentId compId, [])
+
 // assume that the AST is correct (as it will be checked upon creation of the component)
-let rec evaluate (tree: ExprInfo) components : Value * Size= 
+let rec evaluate (tree: ExprInfo) components (sd: SimulationData) step: Value * Size= 
 
     let resizeRes (size: Size) res = 
         match res, size with 
@@ -51,8 +65,8 @@ let rec evaluate (tree: ExprInfo) components : Value * Size=
     let ExprEval (fInt: Option<Functions>) (fUint: Option<Functions>) (fBool: Option<Functions>) ops=
         match ops with
         | BinOp(l, r) ->
-            let leftRes, sizeL = evaluate l components
-            let rightRes, sizeR = evaluate r components 
+            let leftRes, sizeL = evaluate l components sd step
+            let rightRes, sizeR = evaluate r components sd step
 
             let value = 
                 match leftRes, rightRes with
@@ -74,7 +88,7 @@ let rec evaluate (tree: ExprInfo) components : Value * Size=
                 | _ -> failwithf "should not happen" 
             resizeRes sizeL value 
         | UnOp op ->
-            let opEvald, size = evaluate op components
+            let opEvald, size = evaluate op components sd step
 
             match opEvald with
             | Int op ->
@@ -97,18 +111,26 @@ let rec evaluate (tree: ExprInfo) components : Value * Size=
             | Value (Int int)->  Int int
             | Value (Uint uint) -> Uint uint
             | Value (Bool bool) -> Bool bool
-            | Id id -> Int 0 // not implemented yet
+            | Id id -> 
+                let fCompId = getFComponentId id components 
+                let data = sd.FastSim.getSimulationData step fCompId (OutputPortNumber 0)
+                match data with 
+                | Data{Dat = fb; Width = _} ->  
+                    match fb with 
+                    | Word w -> Uint w
+                    | _ -> failwithf "not supported yet"
+                | _ -> failwithf "should not happen"
         let _, size = getLitProperties components lit
         value, Size size
 
     | Cast c, _ ->
         match c with
-        | ToSigned e -> cast e "int" components// this might require some sort of manipulation? or will it be done automatically
-        | ToUnsigned e -> cast e "uint" components
-        | ToBool e -> cast e "bool" components
+        | ToSigned e -> cast e "int" components sd step// this might require some sort of manipulation? or will it be done automatically
+        | ToUnsigned e -> cast e "uint" components sd step
+        | ToBool e -> cast e "bool" components sd step
 
     | BusCast (destSize, e), _-> 
-        let value, _ = evaluate e components
+        let value, _ = evaluate e components sd step
         resizeRes (Size destSize) value
 
     | Add ops, _ -> ExprEval (Some(ItI (+))) (Some(UtU (+))) None ops  
@@ -134,9 +156,9 @@ let rec evaluate (tree: ExprInfo) components : Value * Size=
 
     // what is the difference between and and let inside the linked function
     // i think that it's better probably to do and (for efficiency reasons i wonder)
-and cast expr castType components=
+and cast expr castType components sd step=
     // cast per se can't fail, but the expression it's called on might, so we need to be able to propagate the error
-    let castExprEvaluated, size= evaluate expr components
+    let castExprEvaluated, size= evaluate expr components sd step
     let value = 
         match castExprEvaluated, castType with
         | Int int, "uint" -> Uint(uint int)

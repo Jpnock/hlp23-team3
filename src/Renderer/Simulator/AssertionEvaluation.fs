2d86b88a2cff70389f56b95c9e02097cd175750c
lupisdupis <61511096+lucianecchi@users.noreply.github.com>
2023-03-15 14:47:36 +0000

    reintroduced size evaluation

--- src/Renderer/Simulator/AssertionEvaluation.fs
+++ src/Renderer/Simulator/AssertionEvaluation.fs
@@ -6,6 +6,7 @@ open AssertionCheck
 open CommonTypes 
 open SimulatorTypes
 open BusWidthInferer
+open System
 
 // these types are here because they concern evaluation, not assertions in general
 type IntToBool = int -> int -> bool
@@ -40,6 +41,17 @@ let boolToUint =
 let intToBool n = if n = 0 then false else true
 let uintToBool n = if n = uint 0 then false else true
 
+let getLitMinSize lit = 
+    match lit with
+    | Int intN -> 
+        let n = float intN
+        (ceil >> int) (Math.Log (n, 2.) + 0.1) + 1
+    | Uint uint -> 
+        let n = float uint 
+        (ceil >> int) (Math.Log (n, 2.) + 0.1)
+    | Bool bool -> 1 //technically not needed but will be returned, otherwise can put size as an option bt a bit of a pain
+
+
 /// get FComponentId for component that is in the sheet where it's currently being simulated 
 let getFComponentId label components = 
     let isRightComponent (comp: FastComponent) = 
@@ -60,7 +72,7 @@ let getLitType (components: FastComponent List) lit =
 
 
 // assume that the AST is correct (as it will be checked upon creation of the component)
-let rec evaluate (tree: ExprInfo) (fs:FastSimulation) step connectionsWidth: Result<Value, string> =
+let rec evaluate (tree: ExprInfo) (fs:FastSimulation) step (connectionsWidth: ConnectionsWidth): Result<Value * Size, string> =
     let resizeRes (size: Size) res = 
         match res, size with 
         | Int neg, Size s when neg < 0 -> Int (max neg (int (-(2. ** float (s- 1)))))
@@ -72,7 +84,7 @@ let rec evaluate (tree: ExprInfo) (fs:FastSimulation) step connectionsWidth: Res
     let ExprEval (fInt: Option<Functions>) (fUint: Option<Functions>) (fBool: Option<Functions>) ops=
         match ops with
         | BinOp(l, r) ->
-            let leftRes = evaluate l fs step connectionsWidth 
+            let leftRes= evaluate l fs step connectionsWidth 
             let rightRes= evaluate r fs step connectionsWidth
             printf "evaluated %A %A" leftRes rightRes
             printf "from %A %A" l r
@@ -80,29 +92,29 @@ let rec evaluate (tree: ExprInfo) (fs:FastSimulation) step connectionsWidth: Res
             let value = 
                 match leftRes, rightRes with
                 // all this repeated code is required as type inference does not allow to put the different conditions as the same one 
-                | Ok(Int op1), Ok(Int op2) -> 
+                | Ok(Int op1, Size sizeL), Ok(Int op2, Size sizeR) -> 
                     match fInt with
-                    | Some(ItB f) -> Ok(Bool(f  op1 op2))
-                    | Some(ItI f) -> Ok(Int(f op1 op2))
+                    | Some(ItB f) -> Ok(Bool(f  op1 op2), Size 1)
+                    | Some(ItI f) -> Ok(Int(f op1 op2), Size(max sizeL sizeR))
                     | _ -> Error("Dev error: no function provided for the needed type")
-                | Ok(Int op1), Ok(Uint op2) -> 
+                | Ok(Int op1, Size sizeL), Ok(Uint op2, Size sizeR) -> 
                     match fInt with
-                    | Some(ItB f) -> Ok(Bool(f op1 (int op2)))
-                    | Some(ItI f) -> Ok(Int(f op1 (int op2)))
+                    | Some(ItB f) -> Ok(Bool(f op1 (int op2)), Size 1)
+                    | Some(ItI f) -> Ok(Int(f op1 (int op2)), Size(max sizeL sizeR))
                     | _ -> Error("Dev error: no function provided for the needed type")
-                | Ok(Uint op1), Ok(Int op2) ->
+                | Ok(Uint op1, Size sizeL), Ok(Int op2, Size sizeR) ->
                     match fInt with
-                    | Some(ItB f) -> Ok(Bool(f (int op1) op2))
-                    | Some(ItI f) -> Ok(Int(f (int op1) op2))
+                    | Some(ItB f) -> Ok(Bool(f (int op1) op2), Size(1))
+                    | Some(ItI f) -> Ok(Int(f (int op1) op2), Size(max sizeL sizeR))
                     | _ -> Error("Dev error: no function provided for the needed type")
-                | Ok(Uint op1), Ok(Uint op2) ->
+                | Ok(Uint op1, Size sizeL), Ok(Uint op2, Size sizeR) ->
                     match fUint with
-                    | Some(UtB f) -> Ok(Bool(f op1 op2))
-                    | Some(UtU f) -> Ok(Uint(f op1 op2))
+                    | Some(UtB f) -> Ok(Bool(f op1 op2), Size(1))
+                    | Some(UtU f) -> Ok(Uint(f op1 op2), Size(max sizeL sizeR))
                     | _ -> Error("Dev error: no function provided for the needed type")
-                | Ok(Bool op1), Ok(Bool op2) ->
+                | Ok(Bool op1, Size sizeL), Ok(Bool op2, Size sizeR) ->
                     match fBool with
-                    | Some(BtB f) -> Ok(Bool(f op1 op2))
+                    | Some(BtB f) -> Ok(Bool(f op1 op2), Size(max sizeL sizeR))
                     | _ -> Error("Dev error: no function provided for the needed type")
                 | Error(e1), Error(e2) -> Error(e1+e2)
                 | Error(e1), _ -> Error(e1)
@@ -113,36 +125,37 @@ let rec evaluate (tree: ExprInfo) (fs:FastSimulation) step connectionsWidth: Res
             let opEvald = evaluate op fs step connectionsWidth
 
             match opEvald with
-            | Ok(Int op) ->
+            | Ok(Int op, size) ->
                 match fInt with
-                | Some(ItIUn f) -> Ok(Int(f op))
+                | Some(ItIUn f) -> Ok(Int(f op), size)
                 | _ -> Error("should not happen")
-            | Ok(Uint op) ->
+            | Ok(Uint op, size) ->
                 match fUint with
-                | Some(UtUUn f) -> Ok(Uint(f op))
+                | Some(UtUUn f) -> Ok(Uint(f op), size)
                 | _ ->Error("should not happen" )
-            | Ok(Bool op)->
+            | Ok(Bool op, size)->
                 match fBool with
-                | Some(BtBUn f) -> Ok(Bool(f op))
+                | Some(BtBUn f) -> Ok(Bool(f op), Size 1)
                 | _ -> Error("should not happen")
     
     match tree with
     | Lit lit, _ ->
         let value = 
             match lit with
-            | Value (Int int)->  Int int
-            | Value (Uint uint) -> Uint uint
-            | Value (Bool bool) -> Bool bool
-            | Id (id, portNumber, _) -> 
+            | Value (Int int)->  Ok(Int int, Size (getLitMinSize (Int int)))
+            | Value (Uint uint) -> Ok(Uint uint, Size (getLitMinSize (Uint uint)))
+            | Value (Bool bool) -> Ok(Bool bool, Size 1)
+            | Id (id, portNumber, connId) -> 
                 let fCompId = getFComponentId id (List.ofSeq fs.FComps.Values)
                 let data = fs.getSimulationData step fCompId (OutputPortNumber portNumber)
-                match data with 
-                | Data{Dat = fb; Width = _} ->  
+                let size = connectionsWidth[ConnectionId connId]
+                match data, size with 
+                | Data{Dat = fb; Width = _}, Some(width) ->  
                     match fb with 
-                    | Word w -> Uint w
-                    | _ -> failwithf "not supported yet"
-                | _ -> failwithf "should not happen"
-        Ok(value)
+                    | Word w -> Ok(Uint w, Size width)
+                    | _ -> Error("failed to retrieve width or value of literal")
+                | _ -> Error("dev error")
+        value
 
     | Cast c, _ ->
         match c with
@@ -163,8 +176,8 @@ let rec evaluate (tree: ExprInfo) (fs:FastSimulation) step connectionsWidth: Res
             let left = evaluate l fs step connectionsWidth
             let right = evaluate r fs step connectionsWidth 
             match left, right with 
-            | _, Ok(Int 0) -> Error("division by 0 attempted")
-            | _, Ok(Uint 0u) -> Error("division by 0 attempted")
+            | _, Ok(Int 0, _) -> Error("division by 0 attempted")
+            | _, Ok(Uint 0u, _) -> Error("division by 0 attempted")
             | _, _ -> ExprEval (Some(ItI (/))) (Some(UtU (/))) None ops  
     | Rem ops, _ -> ExprEval (Some(ItI (%))) (Some(UtU (%))) None ops  
     | BitAnd ops, _ -> ExprEval (Some(ItI (&&&))) (Some(UtU (&&&))) None ops 
@@ -190,12 +203,12 @@ and cast expr castType fs step connectionsWidth=
     let castExprEvaluated= evaluate expr fs step connectionsWidth
     let value = 
         match castExprEvaluated, castType with
-        | Ok(Int int), "uint" -> Ok(Uint(uint int))
-        | Ok(Int int), "bool" -> Ok(Bool(intToBool int))
-        | Ok(Bool bool), "uint" -> Ok(Uint(boolToUint bool))
-        | Ok(Bool bool), "int" -> Ok(Int(boolToInt bool))
-        | Ok(Uint uint), "int" -> Ok(Int(int uint))
-        | Ok(Uint uint), "bool" -> Ok(Bool(uintToBool uint))
+        | Ok(Int int, size ), "uint" -> Ok(Uint(uint int), size)
+        | Ok(Int int, size), "bool" -> Ok(Bool(intToBool int), size)
+        | Ok(Bool bool, size), "uint" -> Ok(Uint(boolToUint bool), size)
+        | Ok(Bool bool, size), "int" -> Ok(Int(boolToInt bool), size)
+        | Ok(Uint uint, size), "int" -> Ok(Int(int uint), size)
+        | Ok(Uint uint, size), "bool" -> Ok(Bool(uintToBool uint), size)
         | _, _ -> castExprEvaluated
     value
 //maybe make r an option to reduce the code? if it's present do binary else unary
@@ -214,21 +227,24 @@ type FailedAssertion = {
 
 //function created by Lu for now will have place holder of fake data
 let evaluateAssertionsInWindow (startCycle : int) (endCycle : int) (fs: FastSimulation): FailedAssertion list =
-    let connectionsWidth: ConnectionsWidth list  = 
+    let connectionsWidth: ConnectionsWidth= 
         fs.SimulatedCanvasState
         |> List.map (fun lC -> inferConnectionsWidth lC.CanvasState)
         |> List.collect (function 
             | Ok cw -> [cw] 
             | Error _ -> []) 
+        |> List.collect (fun map -> Map.toList map)
+        |> Map.ofList
+        // need to collect the maps in one map
 
     let evalTree step assertion = 
         let value= evaluate assertion.AssertExpr fs step connectionsWidth 
         match value with 
-        | Ok(Bool true) -> None 
-        | Ok(Bool false) ->
+        | Ok(Bool true, _) -> None 
+        | Ok(Bool false, _) ->
             let prettyAST = AssertionParser.prettyPrintAST (fst assertion.AssertExpr) "" false
             Some {Cycle = step; FailureMessage = $"The assertion \n{prettyAST}\nwas supposed to return true but it returned false\n"; Sheet = "Not implemented"} 
-        | Ok(e) -> 
+        | Ok(e, _) -> 
             let prettyAST = AssertionParser.prettyPrintAST (fst assertion.AssertExpr) "" false
             Some {Cycle = step; FailureMessage = $"The assertion \n{prettyAST}\nwas supposed to return a bool but it returned: {e}s\n"; Sheet = "Not implemented"} 
         | Error(e) -> 
